39becdf296a2cb240f35f078231c618620c91b50
AngelHaven300 <angegabriel369@gmail.com>
2023-11-01 20:33:12 -0400

    average

---
+++ introductions.py
@@ -0,0 +1,13 @@
+#Write a function that takes two pieces of input (2people) and introduces one to another 
+#prompt the user for 2 names 
+#call ur function, which should then print an introduction 
+
+def introduction(name1, name2):
+    print("hi " + " " + name1 + "this is" + " " + name2)
+
+name1 = input("give me a name")
+name2 = input("give me another name")
+
+introduction(name1, name2)
+
+#name1 and name2 are the arguments
